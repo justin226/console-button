344e6095d793ff3a70f1f1751361e69cec634e3a
Justin Fu <56492118+justin226@users.noreply.github.com>
2020-09-14 14:24:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 var consolediv = document.createElement("console");
-consolediv.id = "console";
-consolediv.innerHTML = "<div id=\"consoleheader\"><button id=\"closeconsole\">X</button></div><div id=\"consolehistory\"</div>&gt;<textarea id=\"cmd\">"
+consolediv.id = "div";
+consolediv.innerHTML = "<div id=\"consoleheader\"><button id=\"closeconsole\">X</button></div><div id=\"consolehistory\"</div>&gt;<textarea rows=\"1\" id=\"cmd\">"
 var consolecss = document.createElement("style");
-consolecss.innerHTML = "#console{position:absolute;z-index:9;background-color:#f1f1f1;border:1px solid #d3d3d3;text-align:center}#consoleheader{padding:10px;cursor:move;z-index:10;background-color:#2196f3;color:#fff}";
+consolecss.innerHTML = "#console{position:absolute;z-index:9;background-color:#f1f1f1;border:1px solid #d3d3d3;text-align:center}#consoleheader{padding:10px;cursor:move;z-index:10;background-color:#2196f3;color:#fff;text-align:right;}";
 document.head.appendChild(consolecss);
 document.body.appendChild(consolediv);
 dragElement(document.getElementById("console"));
 document.getElementById("closeconsole").addEventListener("click", function(){
   document.getElementById("console").style.display = "none";
+});
+document.getElementById("cmd").addEventListener("keypress", function(e){
+  if(e.keyCode == 13) {
+    var result;
+    try{result = eval(document.getElementById("cmd").value)} catch (err) {result = "Uncaught" + err };
+    document.getElementById("consolehistory").innerHTML += "<br>" + result;
+    document.getElementById("cmd").value = "";
+  }
 });
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
