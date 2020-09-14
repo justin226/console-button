cf6edca998c0a7833958ef010923c6083bafaca4
Justin Fu <56492118+justin226@users.noreply.github.com>
2020-09-14 14:06:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 var consolediv = document.createElement("console");
 consolediv.id = "console";
-consolediv.innerHTML = "<div id="mydivheader"><button id=\"closeconsole\">X</button><div>"
-
+consolediv.innerHTML = "<div id=\"consoleheader\"><button id=\"closeconsole\">X</button></div><div id=\"consolehistory\"</div>&gt;<textarea id=\"cmd\">"
+var consolecss = document.createElement("style");
+consolecss.innerHTML = "#console{position:absolute;z-index:9;background-color:#f1f1f1;border:1px solid #d3d3d3;text-align:center}#consoleheader{padding:10px;cursor:move;z-index:10;background-color:#2196f3;color:#fff}";
+document.head.appendChild(consolecss);
+document.body.appendChild(consolediv);
 dragElement(document.getElementById("console"));
-
+document.getElementById("closeconsole").addEventListener("click", function(){
+  document.getElementById("console").style.display = "none";
+});
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
